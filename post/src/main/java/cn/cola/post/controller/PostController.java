27014874c7c1122fe631702130fbe3567fff53ec
ColaBlack <2698360477@qq.com>
2024-11-02 20:46:40 +0800

    add 一些注释

--- post/src/main/java/cn/cola/post/controller/PostController.java
+++ post/src/main/java/cn/cola/post/controller/PostController.java
@@ -27,6 +27,9 @@ public class PostController {
     @Resource
     private PostService postService;
 
+    /**
+     * 发布帖子
+     */
     @PostMapping("/publish")
     public BaseResponse<Long> publishPost(@RequestBody PublishPostDTO publishPostDTO, @CookieValue(value = "token") String token) {
         UserVO userVO = JwtUtils.verifyAndGetUserVO(token);
@@ -39,6 +42,9 @@ public class PostController {
         return ResultUtils.success(ret);
     }
 
+    /**
+     * 搜索帖子
+     */
     @GetMapping("/search")
     public BaseResponse<Page<PostVO>> searchPost(@RequestParam(value = "keyword") String keyword,
                                                  @RequestParam(value = "page", required = false, defaultValue = "1") Integer page,
@@ -50,6 +56,9 @@ public class PostController {
         return ResultUtils.success(ret);
     }
 
+    /**
+     * 查看帖子详情
+     */
     @GetMapping("/get/vo/{postId}")
     public BaseResponse<PostVO> getPostVO(@PathVariable("postId") Long postId) {
         PostVO ret = postService.getPostDetail(postId);
